cea3ac5f6e83c6a5a487a522339a8176f3a965b6
natpbtn <126801065+natpbtn@users.noreply.github.com>
2023-03-08 14:11:44 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,57 @@
+package Day4b.bai42;
+
+import java.awt.geom.Area;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        if(radius <= 0){
+            throw new RuntimeException("Error");
+        }
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        if(radius <= 0){
+            throw new RuntimeException("Error");
+        }
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            throw new RuntimeException("Error");
+        }
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" +
+                "radius=" + radius +
+                ",color='" + color + '\'' +
+                ']';
+    }
+}
